761cabbea620b3ed1b6765fd60278f9439d3dc67
Consultant <consultant@Admins-MacBook-Pro.local>
2022-04-01 22:11:04 -0400

    Navigation buttons between activities working, both -create new account- -back- buttons are operating correctly

--- app/src/main/java/com/javacodegeeks/android/android_engineer_exercise/CreateAccount.java
+++ app/src/main/java/com/javacodegeeks/android/android_engineer_exercise/CreateAccount.java
@@ -2,13 +2,23 @@ package com.javacodegeeks.android.android_engineer_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CreateAccount extends AppCompatActivity {
+
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        backButton = findViewById(R.id.iv_back_button);
+        backButton.setOnClickListener(view -> {
+            finish();
+        });
     }
 }
--- app/src/main/java/com/javacodegeeks/android/android_engineer_exercise/MainActivity.java
+++ app/src/main/java/com/javacodegeeks/android/android_engineer_exercise/MainActivity.java
@@ -2,13 +2,26 @@ package com.javacodegeeks.android.android_engineer_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_main);
+
+        // Create btn to open the second activity using Intent.
+        btnCreateAccount = findViewById(R.id.btn_create_account);
+        btnCreateAccount.setOnClickListener(view -> {
+            Intent intentCreateAccount = new Intent();
+            intentCreateAccount.setClass(this, CreateAccount.class);
+            startActivity(intentCreateAccount);
+        });
     }
 }
